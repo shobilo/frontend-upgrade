6de135e8dc5789fd457e94ce6b577c217594ac7e
Alex <alex.shabanovich@bk.ru>
2024-11-03 17:18:40 +0300

    Add webpack dev server with env vars usage

---
+++ config/webpackBuild/buildDevServer.ts
@@ -0,0 +1,12 @@
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import { BuildOptions } from "./types/config";
+
+export const buildDevServer = (
+  options: BuildOptions
+): DevServerConfiguration => {
+  const { port } = options;
+  return {
+    port,
+    open: true,
+  };
+};
--- config/webpackBuild/buildWebpackConfig.ts
+++ config/webpackBuild/buildWebpackConfig.ts
@@ -1,12 +1,12 @@
 import { Configuration } from "webpack";
-import path from "path";
 import { BuildOptions } from "./types/config";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
 
 export const buildWebpackConfig = (options: BuildOptions): Configuration => {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
   return {
     mode,
     entry: paths.entry,
@@ -21,5 +21,7 @@ export const buildWebpackConfig = (options: BuildOptions): Configuration => {
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : false,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 };

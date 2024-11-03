fc37b103f12c2235d051470bb8a6c81de56e7dca
Alex <alex.shabanovich@bk.ru>
2024-11-03 17:41:56 +0300

    Add react usage and scss loaders

--- config/webpackBuild/buildLoaders.ts
+++ config/webpackBuild/buildLoaders.ts
@@ -1,10 +1,23 @@
 import { RuleSetRule } from "webpack";
 
 export const buildLoaders = (): RuleSetRule[] => {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  // without ts we should use babel-loader for the jsx
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [typescriptLoader];
+  return [typescriptLoader, cssLoader];
 };
---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+import { render } from "react-dom";
+
+render(<div>hello</div>, document.getElementById("root"));
